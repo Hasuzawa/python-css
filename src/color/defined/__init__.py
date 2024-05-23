0fb0277b53f34882dbd0da91dd6784e243107d58
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2024-05-23 19:47:38 +0900

    add: common color

---
+++ src/color/defined/__init__.py
@@ -0,0 +1,23 @@
+"""Basic colors as defined by CSS Color Module Level 3
+
+See details at
+TODO
+"""
+from color import Color
+
+Black = Color(0, 0, 0)
+Silver = Color(192, 192, 192)
+Gray = Color(128, 128, 128)
+White = Color(255, 255, 255)
+Maroon = Color(128, 0, 0)
+Red = Color(255, 0, 0)
+Purple =Color(128, 0, 128)
+Fuchsia = Color(255, 0, 255)
+Green = Color(0, 128, 0)
+Lime = Color(0, 255, 0)
+Olive = Color(128, 128, 0)
+Yellow = Color(255, 255, 0)
+Navy = Color(0, 0, 128)
+Blue = Color(0, 0, 255)
+Teal = Color(0, 128, 128)
+Aqua = Color(0, 255, 255)
