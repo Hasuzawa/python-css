1503f5674365160acd69b04ba44284eb85769821
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2024-05-23 20:55:37 +0900

    add: doc for color

--- src/color/color.py
+++ src/color/color.py
@@ -1,8 +1,10 @@
-from typing import Self, overload
+from typing import overload
+
 from .conversion import hex2rgb
 
 
 class Color:
+
     def __init__(self, r: int, g: int, b: int, *, alpha: float | None = None):
         self._r = r
         self._g = g
@@ -22,8 +24,21 @@ class Color:
         return self._b
 
     @property
+    def opacity(self) -> float | None:
+        return self._alpha
+
+    @property
     def alpha(self) -> float | None:
         return self._alpha
+
+    def hex(self, capitalized: bool = False) -> str:
+        r = hex(self._r).lstrip("0x")
+        g = hex(self._g).lstrip("0x")
+        b = hex(self._b).lstrip("0x")
+        s = "#{r}{g}{b}".format(r=r, g=g, b=b)
+        if capitalized:
+            return s.upper()
+        return s
 
 
 @overload
@@ -34,12 +49,28 @@ def rgb(
     *,
     alpha: float | None = None,
 ) -> Color:
-    """Constructs and returns a `Color` with rgb tuple."""
+    """Constructs and returns a `Color` with rgb values.
+
+    Args:
+        r: Red value of RGB.
+        g: Green value of RGB.
+        b: Blue value of RGB.
+
+    Returns:
+        A RGB color.
+    """
     return Color(r, g, b, alpha=alpha)
 
 
 @overload
 def rgb(hex: str) -> Color:
-    """Constructs and returns a `Color` with hex string."""
+    """Constructs and returns a `Color` with hex string.
+
+    Args:
+        hex: A hex string, e.g. #0d1a3f.
+
+    Returns:
+        A RGB color.
+    """
     r, g, b = hex2rgb(hex)
     return Color(r, g, b, alpha=None)
