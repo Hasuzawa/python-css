426716aa07cb9ac8ef431fcfc737f57a31441080
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2024-05-23 20:53:19 +0900

    add: test for hsl to rgb

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from src.color.conversion import hex2rgb
+from src.color.conversion import hex2rgb, hsl2rgb
 
 
 def get_data() -> dict[str, any]:
@@ -25,4 +25,20 @@ def get_hex_decimal(color):
 )
 def test_hex2rgb(input, output):
     result = hex2rgb(input)
+    assert result == tuple(output)
+
+
+def get_hsl_data():
+    with open("tests/hsl_color.json", "r") as f:
+        return json.load(f)
+
+
+def get_hsl_decimal(color):
+    return color["hsl"], color["rgb"]
+
+
+@pytest.mark.parametrize("input, output", list(map(get_hsl_decimal, get_hsl_data()["colors"])))
+def test_hsl2rgb(input, output):
+    print(input, output)
+    result = hsl2rgb(*input)
     assert result == tuple(output)
